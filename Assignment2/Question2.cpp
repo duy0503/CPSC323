5dfb2cec54d2a24536b73c2c6d1a6c916325be47
tetsutanaka <tetsuya64@csu.fullerton.edu>
2015-09-16 17:26:18 -0700

    Resolved the error check issue by checking if the stack was less than more than 1, not 0. Added documentation but still need to add description for the main function.

--- Assignment2/Question2.cpp
+++ Assignment2/Question2.cpp
@@ -5,13 +5,22 @@
 #include <unordered_map>
 
 using namespace std;
-enum input { letter, underscore, digit, space, op, dollar, etc };
+enum input { letter, underscore, digit, space, op, dollar, etc }; //used in input_validation
 
 bool input_validation(string input);
 void get_variable ( unordered_map<string, int> & variable_map, string w);
 void get_value ( unordered_map<string, int> &variable_map);
 void compute(stack<int> & mystack, char OP);
 int compute_the_expression(unordered_map<string,int> & variable_map, string w, bool* succeeded); 
+
+/*
+Function: main
+Input: None
+Output: None
+Description: Main function.
+
+*/
+
 
 int main() {
 
@@ -48,9 +57,18 @@ int main() {
   return 0;
 }
 
+/*
+Function:input_validation
+Input: string
+Output: bool
+Description: Uses a FA to see if the syntax of the expression is acceptable. 
+It ends when it detects a $, and determines if it is acceptable at that state.
+Before the function begins, it checks if the string ends with a $. If it doesn't it returns false for error.
+*/
+
 bool input_validation(string w) {
   
- int table[6][6] = { 2, 2, 1, 0, 5, 5,
+ int table[6][6] = { 2, 2, 1, 0, 5, 5, //The FA table. There are 6 states. 
 		     5, 5, 1, 3, 4, 5,
 		     2, 2, 2, 3, 4, 5,
 		     2, 2, 1, 3, 4, 5,
@@ -66,7 +84,7 @@ bool input_validation(string w) {
 	  return false;
   }
 
-  while ( i < w.length() )
+  while ( i < w.length() ) //Determines the input for the FA. The enum is declared at the beginning of the file
     {
       if ( isalpha(w[i]) ) //A-Z, a-z = letterif ( (int)w[i] >=
 	{
@@ -134,6 +152,12 @@ bool input_validation(string w) {
     }
 }
 
+/*
+Function: get_variable
+Input: unordered_map with string keys and int values, and a string.
+Output: None
+Description: Parses each named variable in the function, and if it is not already in the unordered_map, it places it there.
+*/
 
 void get_variable ( unordered_map<string, int> & variable_map, string w) {
 
@@ -165,6 +189,14 @@ void get_variable ( unordered_map<string, int> & variable_map, string w) {
   }
 }
 
+/*
+Function: get_value
+Input: unordered_map with string keys and int values
+Output: None
+Description: Asks user to enter values for each variable that is stored in unordered_map.
+*/
+
+
 void get_value ( unordered_map<string, int> &variable_map) {
 
   int value;
@@ -175,6 +207,19 @@ void get_value ( unordered_map<string, int> &variable_map) {
   }
 
 }
+
+/*
+Function: compute_the_expression
+Input: unordered_map with string keys and int values, a string, and a bool pointer
+Output: an int value that represents the outcome of the expression
+Description: Parses the expression for variables, operators, and numbers. 
+When it encounters a variable or number, it pushes its value onto a stack of ints (compute_stack).
+When it encounters an operator, it calls the function compute, which will use the top 2 ints in the stack.
+	If there are less than 2 elements in the stack when it calls compute, it sets the bool to false which will result in an error message in main().
+If, at the end of the function, the stack has more than 1 value, the bool will be set to false.
+These 2 error checks should cover all instances where operators are misplaced or a wrong number of them is used.
+*/
+
 
 int compute_the_expression(unordered_map<string,int> & variable_map, string w, bool* succeeded){
   
@@ -206,18 +251,26 @@ int compute_the_expression(unordered_map<string,int> & variable_map, string w, b
       if ( compute_stack.size() > 1 ) { 
 		  compute(compute_stack, w[i]);
 		}
-	  else *succeeded = false; //if the compute_stack is less than 1, error. 
+	  else { *succeeded = false; } //if the compute_stack is less than 2, error. 
     }
 
   }
-  if (compute_stack.size() > 0)
+  if (compute_stack.size() > 1)
   {
-	  *succeeded = false; //if there are any variables left in the stack at the end of the function, error.
+	  *succeeded = false; //if there are more than one variable left in the stack at the end of the function, error.
   }
   return compute_stack.top();
 
 
 }
+
+/*
+Function: compute
+Input: stack of ints, and a char representing an operator
+Output: None
+Description: Takes the top 2 ints in the stacks and does an arithmetic operation on them based on what the operator char is.
+It then takes the result and pushes it onto the stack.
+*/
 
 void compute(stack<int> & mystack, char OP){
   int result = 0;
