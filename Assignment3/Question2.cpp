46a29c810e01f9eb87c641722f7c013b357a82e4
Flying Oiu <eternaloiu@gmail.com>
2015-09-20 01:42:33 +0900

    Finished Question 2. Still need to add documentation. Added is an image of scratch work I did to get the FA table.

---
+++ Assignment3/Question2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int fatable[4][3] =
+{
+	0, 1, 2,
+	2, 1, 3,
+	0, 3, 3,
+	1, 3, 2
+};
+
+bool fa(string w)
+{
+	int state = 0;
+	char iltr;
+	for (int i = 0; i < w.size(); i++)
+	{
+		iltr = w[i];
+		switch (iltr)
+		{
+		case 'a': state = fatable[state][0];
+			break;
+		case 'b': state = fatable[state][1];
+			break;
+		case 'c': state = fatable[state][2];
+			break;
+		default: return false;
+			break;
+		}
+	}
+	switch (state)
+	{
+	case 0: return false;
+		break;
+	case 1: return true;
+		break;
+	case 2: return true;
+		break;
+	case 3: return false;
+		break;
+	default: return false; 
+		break;
+	}
+}
+
+void process(string w)
+{
+	if (fa(w))
+	{
+		cout << "String " << w << " is accepted by grammar.\n";
+	}
+	else cout << "String << w << " "is not accepted by grammar.\n";
+}
+
+int main()
+{
+	string w1 = "abbbcaaa", w2 = "ccccbbb", w3 = "aabbcbbb"; //all of these should not be accepted.
+	process(w1);
+	process(w2);
+	process(w3);
+	cin.get();
+}
