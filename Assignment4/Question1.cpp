eea704fb4282ba17dfd17ce538fb7a1f864af96e
Duy <ddo@csu.fullerton.edu>
2015-10-01 23:48:26 -0700

    Finished question 1

--- Assignment4/Question1.cpp
+++ Assignment4/Question1.cpp
@@ -1,10 +1,11 @@
 /*
   Author Name: Duy Do, Tetsuya Tanaka
   Course: CPSC 323
-  Assignment 3
+  Assignment 4
   Question 1
-  Due Date: 09/24/2015
-  Project Information: Read a statement and determine whether each token is a number, identifier, a reserved word, or special character
+  Due Date: 10/01/2015
+  Project Information: Read file data.txt and copy it into file newdata.txt by removing all comment lines
+                       extra spaces, and placing space before and after each token
 
  */
 
@@ -30,20 +31,20 @@ const string Special[TOTAL_SPECIAL] = {"=", "_", "*", ";", "(", ")", "<=", "<", 
   Output: None.
 
   Description:
-    - Declare string variable named var to store the current token
+    - Declare string variable named token to store the current token
     - Parse the string w from left to right character by character
       - Whenenver we encounter "//" in the string, skip the rest of the string because it is a comment
       - If the character is one of the digits
-        - As long as the next character is either a number, a letter or underscore, keep adding those characters to var
+        - As long as the next character is either a number, a letter or underscore, keep adding those characters to the token
 	- Check every single charater in var to see if all of them are numbers.
 	  - If they are, then this token is a number. Add the token to the vector 
 	  - Else, it is not identifier ( which is an error ). Add the token to the vector
      
       - If the character is a letter or underscore
-        - Keep looping and adding the next characters to var as long as they are either a digit, a letter or underscore.
-	- If var is "cout", check the next two characters to see if they are "<<".
+        - Keep looping and adding the next characters to token as long as they are either a digit, a letter or underscore.
+	- If token is "cout", check the next two characters to see if they are "<<".
 	   - If they are, then this token is a reserved_word. Add the token to the vector
-        - Loop through each items in the array Reserved_Word to see if var is one of them.
+        - Loop through each items in the array Reserved_Word to see if token is one of them.
            - If it is, then the token is a reserved_word. Add the token to the vector
            - Else, the token is an idenfifier. Add the token to the vector
 
@@ -119,11 +120,10 @@ int main() {
 /* Implementation of function get_tokens */
 void get_tokens(string w, vector<string> &tokens) {
 
-  string var = "";
   int i, j;
   bool not_id = false;
   bool reserved = false;
-  string token;
+  string token = "";
   
   for ( i = 0; i < w.length(); i++ ) {
 
@@ -134,7 +134,7 @@ void get_tokens(string w, vector<string> &tokens) {
     // w[i] is a number
     if ( isdigit(w[i]) ) {
       while ( isalpha(w[i]) || isdigit(w[i]) || w[i] == '_' ) {
-	var = var + w[i];
+	token = token + w[i];
 	i++;
       }
 
@@ -146,41 +146,34 @@ void get_tokens(string w, vector<string> &tokens) {
       }
 
       if ( not_id ) {
-	
-	token = var;
         add_token_to_list(token, tokens);
 	not_id = false;
       }
       else {
-	
-	token = var;
         add_token_to_list(token, tokens);
       }
-      var = "";
     }
 
     //w[i] is a letter or '_'
     if ( isalpha(w[i]) || w[i] == '_' ) {
       while ( isalpha(w[i]) || isdigit(w[i]) || w[i] == '_' ) {
-	var = var + w[i];
+	token = token + w[i];
 	i++;
       }
 
-      //Check if var is "cout"
-      if ( var == "cout" ) {
+      //Check if token is "cout"
+      if ( token == "cout" ) {
 	//check the next two character to see if they are "<<"
 	if ( w[i] == '<' && w[i+1] == '<' ) {
-	  var = var + "<<";
-	  token = var;
+	  token = token + "<<";
 	  add_token_to_list(token, tokens);
 	  i++;
-	  var = "";
 	  continue;
 	}
       }
-      // Check if var is a reserved word
+      // Check if token is a reserved word
       for ( j = 0; j < TOTAL_RESERVED_WORD; j++ ) {
-	if ( var == Reserved_Word[j] ) {
+	if ( token == Reserved_Word[j] ) {
 	  reserved = true;
 	  break;
 	}
@@ -188,36 +181,30 @@ void get_tokens(string w, vector<string> &tokens) {
 
       if ( reserved ) {
 	// token is a reserved word
-	token = var;
         add_token_to_list(token, tokens);
       }
       else {
 	// token is an identifier
-        token = var;
         add_token_to_list(token, tokens);
       }
 
       reserved = false;
-      var = "";
     }
 
     // Check for the special symbol
     if ( w[i] == '<' ) {
-      var = var + w[i];
+      token = token + w[i];
       i++;
       if ( w[i] == '=' ) {
-	var = var + w[i];
+	token = token + w[i];
 	
 	//token is a special symbol
-        token = var;
         add_token_to_list(token, tokens);
       }
       else {
 	//token is not a special symbol
-	token = var;
         add_token_to_list(token, tokens);
       }  
-      var = "";
     }  else {
       for ( j = 0; j < TOTAL_SPECIAL; j++ ) {
 	if ( w[i] == Special[j][0] ) {
