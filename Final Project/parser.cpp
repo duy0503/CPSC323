61418a41f4a700979896da01eef4b991c1bc7bc8
Flying Oiu <eternaloiu@gmail.com>
2015-12-09 08:11:03 -0800

    Finished main part of part 2.

--- Final Project/parser.cpp
+++ Final Project/parser.cpp
@@ -1,34 +1,115 @@
 #include "parser.h"
+#include <deque>
 using namespace std;
 
 /*
-program id num integer + - * / ( ) : Åg write ; end. 
+program id num integer + - * / ( ) : Åg write ; end. ,
 i = identifier
 n = number
 s = string
 */
 
-string table[16][15] = 
+string table[16][16] = 
 {
-	{ "p i ; v 1 b 5 e", "", "", "", "", "", "", "", "", "", "", "", "", "", "" }, // program
-	{ "", "<dec> : <type> ;", "", "", "", "", "", "", "", "", "", "", "", "", "" }, // dec-list
-	{ "", "id X", "", "", "", "", "", "", "", "", "", "", "", "", "" }, // dec
-	{ "", ", <dec>", "", "", "", "", "", "", "", "", "É…", "", "", "", "" }, // X
-	{ "", "", "", "integer", "", "", "", "", "", "", "", "", "", "", "" }, // type
-	{ "", "<stat> Y", "", "", "", "", "", "", "", "", "", "", "<stat> Y", "", "" }, // stat-list
-	{ "", "<stat-list>", "", "", "", "", "", "", "", "", "", "", "<stat-list>", "", "lambda" }, // Y
-	{ "", "<assign>", "", "", "", "", "", "", "", "", "", "", "<write>", "", "" }, // stat
-	{ "", "", "", "", "", "", "", "", "", "", "", "", "write ( <str> id ) ;", "", "" }, // write
-	{ "", "lambda", "", "", "", "", "", "", "", "", "", "s ,", "", "", "" }, // str
-	{ "", "id = <expr> ;", "", "", "", "", "", "", "", "", "", "", "", "", "" }, // assign
-	{ "", "<term> Z", "<term> Z", "", "", "", "", "", "<term> Z", "", "", "", "", "", "" }, // expr
-	{ "", "", "", "", "+ <expr>", "- <expr>", "", "", "", "lambda", "", "", "", "lambda", "" }, // Z
-	{ "", "<factor> <termprime>", "<factor> <termprime>", "", "", "", "", "", "<factor> <termprime>", "", "", "", "", "", "" }, // term
-	{ "", "", "", "", "lambda", "lambda", "* <factor> <termprime>", "/ <factor> <termprime>", "", "lambda", "", "", "", "lambda", "" }, // term prime
-	{ "", "id", "num", "", "", "", "", "", "( <expr> )", "", "", "", "", "", "" } // factor
+	{ "p i ; v 1 b 5 e", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "" }, // program
+	{ "", "2 : 4 ;", "", "", "", "", "", "", "", "", "", "", "", "", "", "" }, // dec-list
+	{ "", "i 3", "", "", "", "", "", "", "", "", "", "", "", "", "", "" }, // dec
+	{ "", "", "", "", "", "", "", "", "", "", "l", "", "", "", "", ", 2" }, // X
+	{ "", "", "", "t", "", "", "", "", "", "", "", "", "", "", "", "" }, // type
+	{ "", "7 6", "", "", "", "", "", "", "", "", "", "", "7 6", "", "", "" }, // stat-list
+	{ "", "5", "", "", "", "", "", "", "", "", "", "", "5", "", "l", "" }, // Y
+	{ "", "10", "", "", "", "", "", "", "", "", "", "", "8", "", "", "" }, // stat
+	{ "", "", "", "", "", "", "", "", "", "", "", "", "w ( 9 i ) ;", "", "", "" }, // write
+	{ "", "l", "", "", "", "", "", "", "", "", "", "s ,", "", "", "", "" }, // str
+	{ "", "i = 11 ;", "", "", "", "", "", "", "", "", "", "", "", "", "", "" }, // assign
+	{ "", "13 12", "13 12", "", "", "", "", "", "13 12", "", "", "", "", "", "", "" }, // expr
+	{ "", "", "", "", "+ 11", "- 11", "", "", "", "l", "", "", "", "l", "", "" }, // Z
+	{ "", "15 14", "15 14", "", "", "", "", "", "15 14", "", "", "", "", "", "", "" }, // term
+	{ "", "", "", "", "l", "l", "* 15 14", "/ 15 14", "", "l", "", "", "", "l", "", "" }, // term prime
+	{ "", "i", "n", "", "", "", "", "", "( 11 )", "", "", "", "", "", "", "" } // factor
 };
 
+int get_col(char token)
+{
+	switch (token)
+	{
+	case 'p': return 0;
+		break;
+	case 'i': return 1;
+		break;
+	case 'n': return 2;
+		break;
+	case 't': return 3;
+		break;
+	case '+': return 4;
+		break;
+	case '-': return 5;
+		break;
+	case '*': return 6;
+		break;
+	case '/': return 7;
+		break;
+	case '(': return 8;
+		break;
+	case ')': return 9;
+		break;
+	case ':': return 10;
+		break;
+	case 's': return 11;
+		break;
+	case 'w': return 12;
+		break;
+	case ';': return 13;
+		break;
+	case 'e': return 14;
+		break;
+	case ',': return 15;
+		break;
+	default: return 16;
+		break;
+	}
+}
 
+void error(char expected)
+{
+	switch (expected)
+	{
+	case 'p': cout << "Error. Expected 'program'.\n";
+		break;
+	case 'i': cout << "Error. Expected identifier.\n";
+		break;
+	case 'n': cout << "Error. Expected number.\n";
+		break;
+	case 't': cout << "Error. Expected 'integer'.\n";
+		break;
+	case '+': cout << "Error. Expected '+'.\n";
+		break;
+	case '-': cout << "Error. Expected '-'.\n";
+		break;
+	case '*': cout << "Error. Expected '*'.\n";
+		break;
+	case '/': cout << "Error. Expected '/'.\n";
+		break;
+	case '(': cout << "Error. Expected '('.\n";
+		break;
+	case ')': cout << "Error. Expected ')'.\n";
+		break;
+	case ':': cout << "Error. Expected ':'.\n";
+		break;
+	case 's': cout << "Error. Expected string.\n";
+		break;
+	case 'w': cout << "Error. Expected 'write'.\n";
+		break;
+	case ';': cout << "Error. Expected ';'.\n";
+		break;
+	case 'e': cout << "Error. Expected 'end.'.\n";
+		break;
+	case ',': cout << "Error. Expected ','.\n";
+		break;
+	case 'v': cout << "Error. Expected 'var'.\n";
+		break;
+	}
+}
 
 char convert_reserved_word ( string reserved ) {
 
@@ -41,11 +122,11 @@ char convert_reserved_word ( string reserved ) {
 
 }
 
-vector<char> get_tokens_lists(vector<string> tokens, vector<string> &id, vector<string> &num, vector<string> &reserved){
+deque<char> get_tokens_lists(vector<string> tokens, deque<string> &id, deque<string> &num, vector<string> &reserved){
 
   string current;
   vector<string>::iterator it;
-  vector<char> new_tokens;
+  deque<char> new_tokens;
   char type;
   bool is_reserved = false;
   for ( it = tokens.begin(); it != tokens.end(); it++) {
@@ -84,27 +165,102 @@ vector<char> get_tokens_lists(vector<string> tokens, vector<string> &id, vector<
   return new_tokens;
 }
 
+void push_as_ints( deque <int>& stack, string input )
+{
+	for (int i = input.size() - 1; i >= 0; i--)
+	{
+		if (input[i] == ' ');
+		else
+		{
+			if (input[i] >= '0' && input[i] <= '9')
+			{
+				if (i != 0 && input[i - 1] == '1')
+				{
+					stack.push_back(10 + input[i] - 48);
+					i--;
+				}
+				else
+				stack.push_back(input[i] - 48);
+			}
+			else
+			{
+				stack.push_back(input[i]);
+			}
+		}
+	}
+}
+
 bool parse(vector<string> tokens){
 
-  vector<string> id;
-  vector<string> num;
+  deque<string> id;
+  deque<string> num;
   vector<string> reserved;
-  vector<char> new_tokens;
+  deque<char> new_tokens;
   new_tokens = get_tokens_lists(tokens, id, num, reserved);
-  vector<string>::iterator it;
+  /*vector<string>::iterator it;
   for ( it = id.begin(); it != id.end(); it++) cout<< *it <<" ";
   cout<<endl;
   for ( it = num.begin(); it != num.end(); it++) cout << *it <<" ";
   cout <<endl;
   for ( it = reserved.begin(); it != reserved.end(); it++) cout << *it <<" ";
-  cout <<endl;
-
+  cout <<endl;*/
+  /*
   vector<char>::iterator i;
   for ( i = new_tokens.begin(); i != new_tokens.end(); i++) {
 
     if ( *i == ';' ) cout << *i<<endl;
     else cout <<*i <<" ";
   }
+  */
+  deque<int> stack;
+  stack.push_back(0);
+  string rule;
+  if (new_tokens.front() != 'p')
+  {
+	  cout << "Error. Expected 'program'.\n";
+	  return true;
+  }
+  while (true)
+  {
+	  if (stack.back() >= 0 && stack.back() <= 15) // if top of stack is a rule 0 - 15
+	  {
+		  rule = table[stack.back()][get_col(new_tokens.front())];
+		  if (rule == "") //return error
+		  {
+			  cout << "Error. Unacceptable grammar. " << stack.back() << ", " << int(new_tokens.front()) << endl;
+			  break;
+		  }
+		  else
+		  {
+			  //cout << rule << " " << new_tokens.front() << endl;
+			  stack.pop_back();
+			  push_as_ints(stack, rule);
+		  }
+	  }
+	  else //if top of stack is a nonterminal
+	  {
+		  if (stack.back() == 'l') //if it's lambda
+		  {
+			  stack.pop_back();
+		  }
+		  else if (stack.back() == new_tokens.front())//if it's other nonterminal and it matches
+		  {
+			  if (stack.back() == 'e')
+			  {
+				  cout << "Accepted.\n";
+				  return true;
+			  }
+			  stack.pop_back();
+			  new_tokens.pop_front();
+		  }
+		  else //return error due to mismatch
+		  {
+			  error(stack.back());
+			  break;
+		  }
+	  }
+  } //end of loop
+
   return true;
 
 }
--- Final Project/parser.h
+++ Final Project/parser.h
@@ -6,6 +6,6 @@
 using namespace std;
 
 bool parse(vector<string> tokens);
-vector<char> get_tokens_lists(vector<string> tokens, vector<string> &id, vector<string> &num, vector<string> &reserved);
+deque<char> get_tokens_lists(vector<string> tokens, deque<string> &id, deque<string> &num, vector<string> &reserved);
 char convert_reserved_word ( string reserved );
 #endif
